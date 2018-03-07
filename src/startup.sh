e9e0335d3e2a7a08bb86169e06fa5ac566b7e2ff
Joseph Larson <jlarso11@columbus-oh.cs.colostate.edu>
2018-03-06 17:48:53 -0700

    update startup script

--- src/startup.sh
+++ src/startup.sh
@@ -1,6 +1,6 @@
 CLASSES=cs455-hw2b/src
 SCRIPT="cd cs455-hw2b/src;
-java cs455.scaling.client.Client 129.82.44.141 4000 4; pause 5"
+java cs455.scaling.client.Client 129.82.44.139 4000 5; pause 5"
 #$1 is the command-line argument
 for ((j=1; j<=1; j++));
 do
